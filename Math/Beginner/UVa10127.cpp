1b01538e76b14c5135b1f5c9da104cc42c02d391
Chen Chongsong <chenchongsong@outlook.com>
2017-04-11 15:21:22 +0800

    Easy-math

---
+++ Math/Beginner/UVa10127.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <cmath>
+using namespace std;
+int main() {
+	int n;
+	while (scanf("%d", &n)!=EOF) {
+		int cnt = 1;
+		int remain = 1 % n;
+		while (remain != 0) {
+			cnt++;
+			remain = (remain*10 + 1) % n;
+		}
+		printf("%d\n", cnt);
+	}
+}
